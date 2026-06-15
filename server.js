7fdc0b6304f968715310e0b870304d0b33a60726
lsebastiani <lorenzosebastiani2410@gmail.com>
2026-06-15 15:15:18 +0200

    Books Operation

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/author');
+const bookRouter = require('./routes/book');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -18,15 +19,13 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
-    tls: true,
-    tlsAllowInvalidCertificates: false,
-});
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.on('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000);
